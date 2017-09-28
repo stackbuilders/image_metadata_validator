54607c06d9e93d81bec269d354247185872f93f7
Juan Carlos Pazmiño <juankpro@outlook.com>
2017-09-28 07:45:16 -0500

    Spec proposals

--- spec/resources/user.rb
+++ spec/resources/user.rb
@@ -1,19 +1,5 @@
 require_relative 'avatar_uploader'
 
 class User < ActiveRecord::Base
-  include ActiveModel::Validations
-
-  mount_uploader :avatar_greater, AvatarUploader
-  mount_uploader :avatar_greater_or_equal, AvatarUploader
-  mount_uploader :avatar_equal, AvatarUploader
-  mount_uploader :avatar_less, AvatarUploader
-  mount_uploader :avatar_less_or_equal, AvatarUploader
-
-  validates :avatar_greater, image_width: { greater_than: 200 }
-  validates :avatar_greater_or_equal,
-            image_width: { greater_than_or_equal_to: 200 }
-  validates :avatar_equal, image_width: { equal_to: 200 }
-  validates :avatar_less, image_width: { less_than: 200 }
-  validates :avatar_less_or_equal,
-            image_width: { less_than_or_equal_to: 200 }
+  mount_uploader :avatar, AvatarUploader
 end
