49e2178c934e20705799e7e125eef3703465cdcf
David Revelo <drevelo@stackbuilders.com>
2017-10-03 13:20:12 -0500

    Improve class method comments

--- lib/image_metadata_validator/validators/image_width_validator.rb
+++ lib/image_metadata_validator/validators/image_width_validator.rb
@@ -12,7 +12,19 @@ module ActiveModel
     end
 
     module HelperMethods
-      # TODO: Improve comments
+      # Validates whether the width of an specified carrierewave image is valid.
+      #
+      #   class User
+      #     include ActiveModel::Validations
+      #
+      #     mount_uploader :avatar, AvatarUploader
+      #     validates_width :avatar, { greater_than: 500 }
+      #   end
+      # * <tt>:greater_than</tt> If set, verifies image width is greater than the provided number.
+      # * <tt>:greater_than_or_equal_to</tt> If set, verifies image width is greater than or equals to the provided number.
+      # * <tt>:equal_to</tt> If set, verifies image width is equal to the provided number.
+      # * <tt>:less_than</tt> If set, verifies image width is less than the provided number.
+      # * <tt>:less_than_or_equal_to</tt> If set, verifies image width is less than or equals to the provided number.
 
       def validates_image_width(*attr_names)
         validates_with ImageWidthValidator, _merge_attributes(attr_names)
